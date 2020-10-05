9d5a96d486cd86451d85bd7712a23eaa0cb9c31b
paradocxstd <twerup@yandex.ru>
2020-10-05 19:01:30 +0300

    RestClient add

--- yobitApi/FormMain.Designer.cs
+++ yobitApi/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace yobitApi
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         ///  Required designer variable.
--- yobitApiBl/RestClient.cs
+++ yobitApiBl/RestClient.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Net.Http;
 
 namespace yobitApiBl
 {
-    public enum httpVerb
+    public enum HttpVerb
     {
         GET,
         POST,
@@ -11,5 +15,46 @@ namespace yobitApiBl
     }
     public class RestClient
     {
+        public string EndPoint { get; set; }
+        public HttpVerb HttpMethod { get; set; }
+
+        public RestClient()
+        {
+            EndPoint = string.Empty;
+            HttpMethod = HttpVerb.GET;
+        }
+
+        public string MakeRequest()
+        {
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(EndPoint);
+            request.Method = HttpMethod.ToString();
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new ApplicationException("error code: " + response.StatusCode);
+                }
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if(responseStream != null)
+                    {  
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }//end of streamReader
+                    }
+
+                }// end of using responseStream
+
+
+            }// end of using response
+
+
+            return strResponseValue;
+        }
+
     }
 }
